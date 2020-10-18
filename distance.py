7899d7645654f03bb9995e77a493a7ac5381b69f
nishtha-kalra <nishtha.kalra25@gmail.com>
2020-10-18 23:53:21 +0100

    Adding class

--- distance.py
+++ distance.py
@@ -3,34 +3,45 @@ from operator import itemgetter
 import urllib.request
 import ast
 
-# approximate radius of earth in km
-R = 6371.009
-url = "https://s3.amazonaws.com/intercom-take-home-test/customers.txt"
-file = urllib.request.urlopen(url)
 
-# GPS coordinates of Dublin office
-lat1 = 53.339428
-lon1 = -6.257664
-lat1 = radians(lat1)
-lon1 = radians(lon1)
+class Invite(object):
+    def __init__(self):
+        return
 
-# list to store details of invitees
-invitees = []
+    def find_distance(self):
+        # approximate radius of earth in km
+        earth_radius = 6371.009
+        url = "https://s3.amazonaws.com/intercom-take-home-test/customers.txt"
+        file = urllib.request.urlopen(url)
 
-for row in file:
-    dict_str = row.decode("UTF-8")
-    mydata = ast.literal_eval(dict_str)
-    lat2 = radians(float(mydata['latitude']))
-    lon2 = radians(float(mydata['longitude']))
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
-    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
-    c = 2 * atan2(sqrt(a), sqrt(1 - a))
-    distance = R * c
-    if distance > 100:
-        invitees.append(mydata)
+        # GPS coordinates of Dublin office
+        lat1 = 53.339428
+        lon1 = -6.257664
+        lat1 = radians(lat1)
+        lon1 = radians(lon1)
 
-# sort the list of dictionary by user id
-invitees = sorted(invitees, key=itemgetter('user_id'))
-for invitee in invitees:
-    print("Name:", invitee['name'], "User id:", invitee['user_id'])
+        # list to store details of invitees
+        invitees = []
+
+        for row in file:
+            dict_str = row.decode("UTF-8")
+            mydata = ast.literal_eval(dict_str)
+            lat2 = radians(float(mydata['latitude']))
+            lon2 = radians(float(mydata['longitude']))
+            dlon = lon2 - lon1
+            dlat = lat2 - lat1
+            a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+            c = 2 * atan2(sqrt(a), sqrt(1 - a))
+            distance = earth_radius * c
+            if distance > 100:
+                invitees.append(mydata)
+
+        # sort the list of dictionary by user id
+        invitees = sorted(invitees, key=itemgetter('user_id'))
+        for invitee in invitees:
+            print("Name:", invitee['name'], "User id:", invitee['user_id'])
+
+
+if __name__ == "__main__":
+    invite = Invite()
+    invite.find_distance()
