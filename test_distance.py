bd1def194941177008eebcf80b5c2fca7811af04
nishtha-kalra <nishtha.kalra25@gmail.com>
2020-10-19 11:13:49 +0100

    Adding unit tests

--- test_distance.py
+++ test_distance.py
@@ -1,12 +1,20 @@
 import unittest
+from math import radians
 from invitation.distance import Invite
 
 
 class TestInvitation(unittest.TestCase):
 
-    def test_output(self):
-        invitation = Invite()
-        a = invitation.get_invitees()
+    def test_dublin_address(self):
+        invite = Invite()
+        self.assertEqual(radians(-6.257664), invite.lon_dublin)
+        self.assertEqual(radians(53.339428), invite.lat_dublin)
+
+    def test_calculate_distance(self):
+        invite = Invite()
+        data = {'latitude': '52.986375', 'user_id': 12, 'name': 'Christina McArdle', 'longitude': '-6.043701'}
+        dist = invite.calculate_distance(data)
+        self.assertEqual(dist, 41.76878450547117)
 
 
 if __name__ == '__main__':
