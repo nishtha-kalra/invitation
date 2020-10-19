29ad418b7b0beb7731c9eeb90cf15c4ed6720197
nishtha-kalra <nishtha.kalra25@gmail.com>
2020-10-19 09:25:41 +0100

    Refactoring

--- distance.py
+++ distance.py
@@ -8,40 +8,47 @@ class Invite(object):
     def __init__(self):
         return
 
-    def find_distance(self):
+    def get_invitees(self):
         # approximate radius of earth in km
         earth_radius = 6371.009
+
         url = "https://s3.amazonaws.com/intercom-take-home-test/customers.txt"
-        file = urllib.request.urlopen(url)
 
         # GPS coordinates of Dublin office
-        lat1 = 53.339428
-        lon1 = -6.257664
-        lat1 = radians(lat1)
-        lon1 = radians(lon1)
+        lat_dublin = 53.339428
+        lon_dublin = -6.257664
+        lat_dublin = radians(lat_dublin)
+        lon_dublin = radians(lon_dublin)
 
         # list to store details of invitees
         invitees = []
 
+        file = urllib.request.urlopen(url)
+
         for row in file:
             dict_str = row.decode("UTF-8")
-            mydata = ast.literal_eval(dict_str)
-            lat2 = radians(float(mydata['latitude']))
-            lon2 = radians(float(mydata['longitude']))
-            dlon = lon2 - lon1
-            dlat = lat2 - lat1
-            a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
-            c = 2 * atan2(sqrt(a), sqrt(1 - a))
-            distance = earth_radius * c
+            customer_data = ast.literal_eval(dict_str)
+            distance = self.calculate_distance(customer_data, earth_radius, lat_dublin, lon_dublin)
             if distance > 100:
-                invitees.append(mydata)
+                invitees.append(customer_data)
 
         # sort the list of dictionary by user id
         invitees = sorted(invitees, key=itemgetter('user_id'))
+        print("Name and User Id of customers within 100km of our Dublin office:")
         for invitee in invitees:
             print("Name:", invitee['name'], "User id:", invitee['user_id'])
+
+    def calculate_distance(self, customer_data, earth_radius, lat_dublin, lon_dublin):
+        lat = radians(float(customer_data['latitude']))
+        lon = radians(float(customer_data['longitude']))
+        delta_lon = lon - lon_dublin
+        delta_lat = lat - lat_dublin
+        a = sin(delta_lat / 2) ** 2 + cos(lat_dublin) * cos(lat) * sin(delta_lon / 2) ** 2
+        c = 2 * atan2(sqrt(a), sqrt(1 - a))
+        distance = earth_radius * c
+        return distance
 
 
 if __name__ == "__main__":
     invite = Invite()
-    invite.find_distance()
+    invite.get_invitees()
